7e4c664af0a0fc4b16fa285b6eb28203c7b5099e
kylepruett <kyle.pruett92@gmail.com>
2026-08-19 20:07:00 -0500

    add public/ads.txt for Google AdSense authorization

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://voxelwire.vercel.app',
+  site: 'https://voxel-wire.vercel.app',
   integrations: [sitemap()],
 
   vite: {
